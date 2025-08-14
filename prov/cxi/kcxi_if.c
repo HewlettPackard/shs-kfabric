46a3019c955d5677a26205b14aed11f5c3891f82
Chuck Fossen <charles.fossen@hpe.com>
2025-08-14 14:25:23 -0500

    Revert "Feature: Use rgroup API instead of svc API"
    This reverts commit c43135df8d99fc8e6da1acd5ffa7884e064f4cdb.
    
    NETCASSINI-7681

--- prov/cxi/kcxi_if.c
+++ prov/cxi/kcxi_if.c
@@ -74,112 +74,6 @@ static int kcxi_if_reset_counters_get(void *data, u64 *value)
 DEFINE_SIMPLE_ATTRIBUTE(kcxi_if_reset_counters_ops, kcxi_if_reset_counters_get,
 			kcxi_if_reset_counters_set, "%llu\n");
 
-int kcxi_alloc_rgroup_rsrcs(struct cxi_dev *cdev, struct kcxi_if *kcxi_if,
-		       const struct kfid_fabric *fabric,
-		       const struct kfi_info *info)
-{
-	int i;
-	int rc;
-	bool dynamic_alloc = false;
-	unsigned int ac_entry_id;
-	struct cxi_rgroup *rgroup;
-	struct kcxi_fabric *kcxi_fab;
-	struct cxi_resource_limits limits[CXI_RESOURCE_MAX];
-	const union cxi_ac_data ac_data = {
-		.uid = __kuid_val(current_euid()),
-	};
-
-	limits[CXI_RESOURCE_AC].max = KCXI_DOM_AC_MAX;
-	limits[CXI_RESOURCE_AC].reserved = address_contexts;
-	limits[CXI_RESOURCE_EQ].max = KCXI_DOM_CQ_MAX;
-	limits[CXI_RESOURCE_EQ].reserved = completion_queues;
-	limits[CXI_RESOURCE_CT].max = 0;
-	limits[CXI_RESOURCE_CT].reserved = 0;
-	limits[CXI_RESOURCE_PTLTE].max = KCXI_DOM_RX_CTX_MAX;
-	limits[CXI_RESOURCE_PTLTE].reserved = receive_contexts;
-	limits[CXI_RESOURCE_TXQ].max = KCXI_DOM_TX_CTX_MAX;
-	limits[CXI_RESOURCE_TXQ].reserved = transmit_contexts;
-	limits[CXI_RESOURCE_TGQ].max = KCXI_DOM_RX_CTX_MAX;
-	limits[CXI_RESOURCE_TGQ].reserved = receive_contexts;
-	limits[CXI_RESOURCE_TLE].max = 0;
-	limits[CXI_RESOURCE_TLE].reserved = 0;
-	limits[CXI_RESOURCE_PE0_LE].max = KCXI_LE_MAX;
-	limits[CXI_RESOURCE_PE0_LE].reserved = KCXI_LE_RES(message_buffers);
-	limits[CXI_RESOURCE_PE1_LE].max = KCXI_LE_MAX;
-	limits[CXI_RESOURCE_PE1_LE].reserved = KCXI_LE_RES(message_buffers);
-	limits[CXI_RESOURCE_PE2_LE].max = KCXI_LE_MAX;
-	limits[CXI_RESOURCE_PE2_LE].reserved = KCXI_LE_RES(message_buffers);
-	limits[CXI_RESOURCE_PE3_LE].max = KCXI_LE_MAX;
-	limits[CXI_RESOURCE_PE3_LE].reserved = KCXI_LE_RES(message_buffers);
-
-	/* Override default resource limit parameters */
-	if (fabric) {
-		kcxi_fab = container_of(fabric, struct kcxi_fabric, fab_fid);
-		if (kcxi_fab->dynamic_rsrc_alloc && info->rx_attr && info->domain_attr) {
-			dynamic_alloc = true;
-			limits[CXI_RESOURCE_EQ].reserved = info->domain_attr->cq_cnt;
-			limits[CXI_RESOURCE_PTLTE].reserved = info->domain_attr->rx_ctx_cnt;
-			limits[CXI_RESOURCE_TXQ].reserved = info->domain_attr->tx_ctx_cnt;
-			limits[CXI_RESOURCE_TGQ].reserved = info->domain_attr->rx_ctx_cnt;
-			limits[CXI_RESOURCE_PE0_LE].reserved = KCXI_LE_RES(info->rx_attr->size * info->domain_attr->rx_ctx_cnt);
-			limits[CXI_RESOURCE_PE1_LE].reserved = KCXI_LE_RES(info->rx_attr->size * info->domain_attr->rx_ctx_cnt);
-			limits[CXI_RESOURCE_PE2_LE].reserved = KCXI_LE_RES(info->rx_attr->size * info->domain_attr->rx_ctx_cnt);
-			limits[CXI_RESOURCE_PE3_LE].reserved = KCXI_LE_RES(info->rx_attr->size * info->domain_attr->rx_ctx_cnt);
-		}
-	}
-
-	LOG_INFO("%s CXI resource reservations",
-		 dynamic_alloc ? "Dynamic" : "Parameter based");
-	LOG_INFO("  ACs: %ld  EQs: %ld  PTEs: %ld  TGQs: %ld  TXQs: %ld  LEs: %ld",
-		 limits[CXI_RESOURCE_AC].reserved,
-		 limits[CXI_RESOURCE_EQ].reserved,
-		 limits[CXI_RESOURCE_PTLTE].reserved,
-		 limits[CXI_RESOURCE_TGQ].reserved,
-		 limits[CXI_RESOURCE_TXQ].reserved,
-		 limits[CXI_RESOURCE_PE0_LE].reserved);
-
-	rgroup = cxi_dev_alloc_rgroup(cdev, NULL);
-	if (IS_ERR(rgroup)) {
-		rc = PTR_ERR(rgroup);
-		pr_debug("Failed to allocate rgroup:%d\n", rc);
-		return rc;
-	}
-
-	cxi_rgroup_set_system_service(rgroup, true);
-	cxi_rgroup_set_name(rgroup, "kfabric-rgroup");
-
-	LOG_INFO("Allocated %s id:%d", cxi_rgroup_name(rgroup),
-	         cxi_rgroup_id(rgroup));
-
-	for (i = CXI_RESOURCE_PTLTE; i < CXI_RESOURCE_MAX; i++) {
-		if (!limits[i].reserved && !limits[i].max)
-			continue;
-
-		rc = cxi_rgroup_add_resource(rgroup, i, &limits[i]);
-		if (rc)
-			goto err;
-	}
-
-	rc = cxi_rgroup_add_ac_entry(rgroup, CXI_AC_UID, &ac_data,
-				     &ac_entry_id);
-	if (rc)
-		goto err;
-
-	rc = cxi_rgroup_enable(rgroup);
-	if (rc)
-		goto err;
-
-	kcxi_if->rgroup = rgroup;
-
-	return 0;
-
-err:
-	cxi_rgroup_dec_refcount(rgroup);
-
-	return rc;
-}
-
-
 /**
  * kcxi_if_alloc() - Allocate a kCXI interface.
  * @dev: Local NIC address the kCXI interface should be allocated against.
@@ -190,11 +84,75 @@ struct kcxi_if *kcxi_if_alloc(unsigned int nic, const struct kfid_fabric *fabric
 {
 	int rc;
 	int lock_idx;
+	int i;
+	bool dynamic_alloc = false;
+	struct kcxi_fabric *kcxi_fab;
 	struct kcxi_if *kcxi_if;
 	struct kcxi_dev *kdev;
 	struct cxi_dev *cdev;
 	char if_debugfs_dir_name[16];
+	struct cxi_rsrc_limits limits = {
+		.acs = {
+			.max = KCXI_DOM_AC_MAX,
+			.res = address_contexts,
+		},
+		.eqs = {
+			.max = KCXI_DOM_CQ_MAX,
+			.res = completion_queues,
+		},
+		.cts = {
+			.max = 0,
+			.res = 0,
+		},
+		.ptes = {
+			.max = KCXI_DOM_RX_CTX_MAX,
+			.res = receive_contexts,
+		},
+		.txqs = {
+			.max = KCXI_DOM_TX_CTX_MAX,
+			.res = transmit_contexts,
+		},
+		.tgqs = {
+			.max = KCXI_DOM_RX_CTX_MAX,
+			.res = receive_contexts,
+		},
+		.tles = {
+			.max = 0,
+			.res = 0,
+		},
+		/* Will actually have res/4 value for les */
+		.les = {
+			.max = KCXI_DOM_BUF_MAX / 4,
+			.res = (message_buffers + 3) / 4,
+		},
+	};
+	struct cxi_svc_desc svc_desc = {
+		 .resource_limits = resource_limits,
+		 .restricted_members = restricted_members,
+		 .members[0] = {
+			 .type = CXI_SVC_MEMBER_UID,
+			 .svc_member.uid = (current_euid()).val,
+		 },
+		 .restricted_vnis = restricted_vnis,
+		 .restricted_tcs = restricted_tcs,
+		 .limits = limits,
+		 .is_system_svc = true,
+	};
+	struct cxi_svc_fail_info fail_info = {};
 	uint16_t auth_key = *(info->domain_attr->auth_key);
+
+	/* Override default resource limit parameters */
+	if (fabric) {
+		kcxi_fab = container_of(fabric, struct kcxi_fabric, fab_fid);
+		if (kcxi_fab->dynamic_rsrc_alloc && info->rx_attr && info->domain_attr) {
+			dynamic_alloc = true;
+			svc_desc.limits.eqs.res = info->domain_attr->cq_cnt;
+			svc_desc.limits.ptes.res = info->domain_attr->rx_ctx_cnt;
+			svc_desc.limits.txqs.res = info->domain_attr->tx_ctx_cnt;
+			svc_desc.limits.tgqs.res = info->domain_attr->rx_ctx_cnt;
+			svc_desc.limits.les.res = ((info->rx_attr->size * info->domain_attr->rx_ctx_cnt) + 3) / 4;
+		}
+	}
 
 	kdev = kcxi_dev_get(nic);
 	if (IS_ERR(kdev)) {
@@ -233,11 +191,25 @@ struct kcxi_if *kcxi_if_alloc(unsigned int nic, const struct kfid_fabric *fabric
 		goto err_unlock;
 	}
 
-	rc = kcxi_alloc_rgroup_rsrcs(cdev, kcxi_if, fabric, info);
+	/* Allocate a CXI Service */
+	LOG_INFO("%s CXI resource reservations",
+		dynamic_alloc ? "Dynamic" : "Parameter based");
+	LOG_INFO("  ACs: %d  EQs: %d  PTEs: %d  TGQs: %d  TXQs: %d  LEs: %d",
+		svc_desc.limits.acs.res, svc_desc.limits.eqs.res, svc_desc.limits.ptes.res,
+		svc_desc.limits.tgqs.res, svc_desc.limits.txqs.res, svc_desc.limits.les.res);
+
+	rc = cxi_svc_alloc(cdev, &svc_desc, &fail_info);
 	if (rc < 0) {
 		LOG_INFO("Unable to reserve requested resources");
+		for (i = 0; i < CXI_RSRC_TYPE_MAX; i++) {
+			if (svc_desc.limits.type[i].res > fail_info.rsrc_avail[i]) {
+				LOG_INFO("  %s requested: %d  %s available: %d",
+					cxi_rsrc_type_to_str(i), svc_desc.limits.type[i].res, cxi_rsrc_type_to_str(i), fail_info.rsrc_avail[i]);
+			}
+		}
 		goto err_unlock;
 	}
+	kcxi_if->svc_id = rc;
 
 	rc = kcxi_get_rx_profile(kcxi_if, auth_key);
 	if (rc && rc != -EEXIST) {
@@ -253,7 +225,8 @@ struct kcxi_if *kcxi_if_alloc(unsigned int nic, const struct kfid_fabric *fabric
 		goto err_free_rx_profile;
 	}
 
-	kcxi_if->lni = cxi_lni_alloc(cdev, cxi_rgroup_id(kcxi_if->rgroup));
+	LOG_INFO("");
+	kcxi_if->lni = cxi_lni_alloc(cdev, kcxi_if->svc_id);
 	if (IS_ERR(kcxi_if->lni)) {
 		rc = PTR_ERR(kcxi_if->lni);
 		goto err_free_tx_profile;
@@ -297,7 +270,7 @@ err_free_tx_profile:
 err_free_rx_profile:
 	kcxi_put_rx_profile(kcxi_if, auth_key);
 err_free_svc:
-	cxi_rgroup_dec_refcount(kcxi_if->rgroup);
+	cxi_svc_destroy(cdev, kcxi_if->svc_id);
 err_unlock:
 	srcu_read_unlock(&kdev->dev_lock, lock_idx);
 	kfree(kcxi_if);
@@ -316,6 +289,7 @@ err:
 int kcxi_if_free(struct kcxi_if *kcxi_if)
 {
 	struct kcxi_dev *kdev;
+	struct cxi_dev *cdev;
 
 	if (!kcxi_if)
 		return -EINVAL;
@@ -333,9 +307,11 @@ int kcxi_if_free(struct kcxi_if *kcxi_if)
 
 	cxi_lni_free(kcxi_if->lni);
 
+	cdev = srcu_dereference(kdev->dev, &kdev->dev_lock);
+
 	kcxi_tx_profiles_cleanup(kcxi_if);
 	kcxi_rx_profiles_cleanup(kcxi_if);
-	cxi_rgroup_dec_refcount(kcxi_if->rgroup);
+	cxi_svc_destroy(cdev, kcxi_if->svc_id);
 
 	kfree(kcxi_if);
 
