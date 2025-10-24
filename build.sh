ebb4ed4d05be829b2b42e06c970d8ef23e180983
Terrence Mitchem <terrence.mitchem@hpe.com>
2025-10-24 08:22:27 -0500

    build: Re-enable Debian builds

--- build.sh
+++ build.sh
@@ -27,5 +27,6 @@ fi
 . ${CE_BUILD_SCRIPT_REPO}/build/sh/rpmbuild/load.sh
 
 setup_dst_env
+
 dst_build_rpm -c ${CE_BUILD_SCRIPT_REPO}/build/configs/${CE_CONFIG_FILE} $@
-# dst_build_deb --yamlfile ${CE_BUILD_SCRIPT_REPO}/build/configs/${CE_CONFIG_FILE} --ps ${PRODUCT} --main-branch "main" $@
+dst_build_deb --yamlfile ${CE_BUILD_SCRIPT_REPO}/build/configs/${CE_CONFIG_FILE} --ps ${PRODUCT} --main-branch "main" $@
